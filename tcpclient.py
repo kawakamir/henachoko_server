fda97a0cc68ebf279aae9ce65b5831d0054d8701
kawakamir <rui.kawakami@gmail.com>
2021-03-14 18:47:36 +0900

    update tcpclient

--- tcpclient.py
+++ tcpclient.py
@@ -15,6 +15,32 @@ class TCPClient:
         try:
             # socketを生成
             client_socket = socket.socket()
-            client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+            client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            # サーバーと接続する
+            print("=== サーバーと接続します ===")
+            client_socket.connect(("localhost", 80))
+            print("=== サーバーとの接続が完了しました　===")
+
+            with open("client_send.txt", "rb") as f:
+                request = f.read()
+
+            # サーバーへリクエストを送信する
+            client_socket.send(request)
+
+            # サーバーからレスポンスが送られてくるのを待ち、取得する
+            response = client_socket.recv(4096)
+
+            # レスポンスの内容を、ファイルに書き出す
+            with open("client_recv.txt", "wb") as f:
+                f.write(response)
+
+            # 通信を終了させる
+            client_socket.close()
+
         finally:
-            pass
+            print("=== クライアントを停止します。 ===")
+
+if __name__ == '__main__':
+    client = TCPClient()
+    client.request()
