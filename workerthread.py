453a479b6880b1208cb444cd29f85ac4cba5345d
kawakamir <rui.kawakami@gmail.com>
2021-03-18 08:24:16 +0900

    add post method

--- workerthread.py
+++ workerthread.py
@@ -17,7 +17,7 @@ class WorkerThread(Thread):
 
     # 拡張子とMIME Typeの対応
     MIME_TYPES = {
-        "html": "text/html",
+        "html": "text/html; charset=UTF-8",
         "css": "text/css",
         "png": "image/png",
         "jpg": "image/jpg",
@@ -63,7 +63,7 @@ class WorkerThread(Thread):
                 response_body = textwrap.dedent(html).encode()
 
                 # Content-Typeを指定
-                content_type = "text/html"
+                content_type = "text/html; charset=UTF-8"
 
                 # レスポンスラインを生成
                 response_line = "HTTP/1.1 200 OK\r\n"
@@ -88,7 +88,7 @@ class WorkerThread(Thread):
                 response_body = textwrap.dedent(html).encode()
 
                 # Content-Typeを指定
-                content_type = "text/html"
+                content_type = "text/html; charset=UTF-8"
 
                 # レスポンスラインを生成
                 response_line = "HTTP/1.1 200 OK\r\n"
@@ -108,7 +108,7 @@ class WorkerThread(Thread):
                     traceback.print_exc()
 
                     response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-                    content_type = "text/html"
+                    content_type = "text/html; charset=UTF-8"
                     response_line = "HTTP/1.1 404 Not Found\r\n"
 
             # レスポンスヘッダーを生成
