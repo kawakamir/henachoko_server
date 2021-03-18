7413ebc731fdd91cf745060ed0766f5a09918b57
kawakamir <rui.kawakami@gmail.com>
2021-03-18 20:47:42 +0900

    refactoring views.py

--- views.py
+++ views.py
@@ -5,7 +5,13 @@ from pprint import pformat
 from typing import Tuple, Optional
 
 
-def now() -> Tuple[bytes, Optional[str], str]:
+def now(
+    method: str,
+    path: str,
+    http_version: str,
+    request_header: dict,
+    request_body: bytes,
+) -> Tuple[bytes, Optional[str], str]:
     """
     現在時刻を表示するHTMLを生成する
     """
@@ -64,6 +70,9 @@ def show_request(
 
 def parameters(
     method: str,
+    path: str,
+    http_version: str,
+    request_header: dict,
     request_body: bytes,
 ) -> Tuple[bytes, Optional[str], str]:
     """
--- workerthread.py
+++ workerthread.py
@@ -24,6 +24,13 @@ class WorkerThread(Thread):
         "gif": "image/gif",
     }
 
+    # pathとview関数の対応
+    URL_VIEW = {
+        "/now": views.now,
+        "/show_request": views.show_request,
+        "/parameters": views.parameters,
+    }
+
     def __init__(self, client_socket: socket, address: Tuple[str, int]):
         super().__init__()
 
@@ -52,16 +59,12 @@ class WorkerThread(Thread):
             content_type: Optional[str]
             response_line: str
 
-            if path == "/now":
-                response_body, content_type, response_line = views.now()
-
-            elif path == "/show_request":
-                response_body, content_type, response_line = views.show_request(
+            # pathに対応するview関数があれば、関数を取得して呼び出し、レスポンスを生成する
+            if path in self.URL_VIEW:
+                view = self.URL_VIEW[path]
+                response_body, content_type, response_line = view(
                     method, path, http_version, request_header, request_body
                 )
-
-            elif path == "/parameters":
-                response_body, content_type, response_line = views.parameters(method, request_body)
 
             # pathがそれ以外のときは、静的ファイルからレスポンスを生成する
             else:
