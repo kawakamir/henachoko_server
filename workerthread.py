ef6d863004e99011ff693b0ae47e1fbe3f52764b
kawakamir <rui.kawakami@gmail.com>
2021-03-18 20:45:20 +0900

    create views module

--- workerthread.py
+++ workerthread.py
@@ -1,13 +1,12 @@
 import os
 import re
-import textwrap
 import traceback
-import urllib.parse
 from datetime import datetime
-from pprint import pformat
 from socket import socket
 from threading import Thread
 from typing import Tuple, Optional
+
+import views
 
 
 class WorkerThread(Thread):
@@ -52,71 +51,17 @@ class WorkerThread(Thread):
             response_body: bytes
             content_type: Optional[str]
             response_line: str
-            # pathが/nowのときは、現在時刻を表示するHTMLを生成する
+
             if path == "/now":
-                html = f"""\
-                    <html>
-                    <body>
-                        <h1>Now: {datetime.now()}</h1>
-                    </body>
-                    </html>
-                """
-                response_body = textwrap.dedent(html).encode()
+                response_body, content_type, response_line = views.now()
 
-                # Content-Typeを指定
-                content_type = "text/html; charset=UTF-8"
-
-                # レスポンスラインを生成
-                response_line = "HTTP/1.1 200 OK\r\n"
-
-            # pathが/show_requestのときは、HTTPリクエストの内容を表示するHTMLを生成する
             elif path == "/show_request":
-                html = f"""\
-                    <html>
-                    <body>
-                        <h1>Request Line:</h1>
-                        <p>
-                            {method} {path} {http_version}
-                        </p>
-                        <h1>Headers:</h1>
-                        <pre>{pformat(request_header)}</pre>
-                        <h1>Body:</h1>
-                        <pre>{request_body.decode("utf-8", "ignore")}</pre>
-
-                    </body>
-                    </html>
-                """
-                response_body = textwrap.dedent(html).encode()
-
-                # Content-Typeを指定
-                content_type = "text/html; charset=UTF-8"
-
-                # レスポンスラインを生成
-                response_line = "HTTP/1.1 200 OK\r\n"
+                response_body, content_type, response_line = views.show_request(
+                    method, path, http_version, request_header, request_body
+                )
 
             elif path == "/parameters":
-                if method == "GET":
-                    response_body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-                    content_type = "text/html; charset=UTF-8"
-                    response_line = "HTTP/1.1 405 Method Not Allowed\r\n"
-
-                elif method == "POST":
-                    post_params = urllib.parse.parse_qs(request_body.decode())
-                    html = f"""\
-                        <html>
-                        <body>
-                            <h1>Parameters:</h1>
-                            <pre>{pformat(post_params)}</pre>                        
-                        </body>
-                        </html>
-                    """
-                    response_body = textwrap.dedent(html).encode()
-
-                    # Content-Typeを指定
-                    content_type = "text/html; charset=UTF-8"
-
-                    # レスポンスラインを生成
-                    response_line = "HTTP/1.1 200 OK\r\n"
+                response_body, content_type, response_line = views.parameters(method, request_body)
 
             # pathがそれ以外のときは、静的ファイルからレスポンスを生成する
             else:
