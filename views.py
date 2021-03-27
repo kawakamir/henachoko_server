1c581abc863105871193436e64d98867395dcadc
kawakamir <rui.kawakami@gmail.com>
2021-03-27 11:38:13 +0900

    update templates

--- views.py
+++ views.py
@@ -1,4 +1,3 @@
-import textwrap
 import urllib.parse
 from datetime import datetime
 from pprint import pformat
@@ -22,21 +21,8 @@ def show_request(request: HTTPRequest) -> HTTPResponse:
     """
     HTTPリクエストの内容を表示するHTMLを生成する
     """
-    html = f"""\
-        <html>
-        <body>
-            <h1>Request Line:</h1>
-            <p>
-                {request.method} {request.path} {request.http_version}
-            </p>
-            <h1>Headers:</h1>Ò
-            <pre>{pformat(request.headers)}</pre>
-            <h1>Body:</h1>
-            <pre>{request.body.decode("utf-8", "ignore")}</pre>
-        </body>
-        </html>
-    """
-    body = textwrap.dedent(html).encode()
+    context = {"request": request, "headers": pformat(request.headers), "body": request.body.decode("utf-8", "ignore")}
+    body = render("show_request.html", context)
 
     return HTTPResponse(body=body)
 
@@ -49,38 +35,19 @@ def parameters(request: HTTPRequest) -> HTTPResponse:
     # GETリクエストの場合は、405を返す
     if request.method == "GET":
         body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-        content_type = "text/html; charset=UTF-8"
-        status_code = 405
+
+        return HTTPResponse(body=body, status_code=405)
 
     elif request.method == "POST":
-        post_params = urllib.parse.parse_qs(request.body.decode())
-        html = f"""\
-            <html>
-            <body>
-                <h1>Parameters:</h1>
-                <pre>{pformat(post_params)}</pre>                        
-            </body>
-            </html>
-        """
-        body = textwrap.dedent(html).encode()
-        content_type = "text/html; charset=UTF-8"
-        status_code = 200
+        context = {"params": urllib.parse.parse_qs(request.body.decode())}
+        body = render("parameters.html", context)
 
-    return HTTPResponse(body=body, content_type=content_type, status_code=status_code)
+        return HTTPResponse(body=body)
 
 
 def user_profile(request: HTTPRequest) -> HTTPResponse:
-    user_id = request.params["user_id"]
-    html = f"""\
-        <html>
-        <body>
-            <h1>プロフィール</h1>
-            <p>ID: {user_id}
-        </body>
-        </html>
-    """
-    body = textwrap.dedent(html).encode()
-    content_type = "text/html; charset=UTF-8"
-    status_code = 200
+    context = {"user_id": request.params["user_id"]}
 
-    return HTTPResponse(body=body, content_type=content_type, status_code=status_code)
+    body = render("user_profile.html", context)
+
+    return HTTPResponse(body=body)
