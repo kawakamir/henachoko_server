e89e5f2194589855d87b0a40133ac6de81441f6f
kawakamir <rui.kawakami@gmail.com>
2021-03-28 17:59:00 +0900

    update set cookie

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+
 import urllib.parse
 from datetime import datetime
 from pprint import pformat
@@ -54,7 +55,7 @@ def user_profile(request: HTTPRequest) -> HTTPResponse:
 
 
 def set_cookie(request: HTTPRequest) -> HTTPResponse:
-    return HTTPResponse(headers={"Set-Cookie": "username=TARO"})
+    return HTTPResponse(cookies={"username": "TARO"})
 
 
 def login(request: HTTPRequest) -> HTTPResponse:
@@ -65,34 +66,21 @@ def login(request: HTTPRequest) -> HTTPResponse:
     elif request.method == "POST":
         post_params = urllib.parse.parse_qs(request.body.decode())
         username = post_params["username"][0]
+        email = post_params["email"][0]
 
-        headers = {"Location": "/welcome", "Set-Cookie": f"username={username}"}
-        return HTTPResponse(status_code=302, headers=headers)
+        return HTTPResponse(
+            status_code=302, headers={"Location": "/welcome"}, cookies={"username": username, "email": email}
+        )
 
 
 def welcome(request: HTTPRequest) -> HTTPResponse:
-    cookie_header = request.headers.get("Cookie", None)
-
-    # Cookieが送信されてきていなければ、ログインしていないとみなして/loginへリダイレクト
-    if not cookie_header:
-        return HTTPResponse(status_code=302, headers={"Location": "/login"})
-
-    # str から list へ変換
-    # ex) "name1=value1; name2=value2" => ["name1=value1", "name2=value2"]
-    cookie_strings = cookie_header.split("; ")
-
-    # list から dict へ変換
-    # ex) ["name1=value1", "name2=value2"] => {"name1": "value1", "name2": "value2"}
-    cookies = {}
-    for cookie_string in cookie_strings:
-        name, value = cookie_string.split("=", maxsplit=1)
-        cookies[name] = value
-
     # Cookieにusernameが含まれていなければ、ログインしていないとみなして/loginへリダイレクト
-    if "username" not in cookies:
+    if "username" not in request.cookies:
         return HTTPResponse(status_code=302, headers={"Location": "/login"})
 
     # Welcome画面を表示
-    body = render("welcome.html", context={"username": cookies["username"]})
+    username = request.cookies["username"]
+    email = request.cookies["email"]
+    body = render("welcome.html", context={"username": username, "email": email})
 
     return HTTPResponse(body=body)
